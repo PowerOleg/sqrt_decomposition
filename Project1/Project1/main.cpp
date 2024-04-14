e610c102f4d6671e2667409445da431dc6356d9c
PowerOleg <geraskin.oleg@mail.ru>
2024-04-14 19:34:20 +0300

    preprocessing

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
 //The task is to find the min value between index 1(included) and index 7(not included)
-int getMin(int* arr, int size, int left_index, int right_index, int* sqrt_memory) {
-    int result = -1;
-    int left = 0;
-    int right = size - 1;
-    while (true)
+
+int get_min_of_block(int* arr, int block_number, const int block_length)
+{
+    int min = 2140000000;//TODO
+    int size = block_number * block_length;
+    for (int i = (block_number - 1) * block_length; i < size; i++)
     {
-
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
     }
-
-    return result;
+    return min;
 }
 
-int main() {
+void preprocessing(int* arr, int* sqrt_memory, int blocks_quantity, const int block_length)
+{
+    for (int block_number = 1; block_number <= blocks_quantity; block_number++)
+    {
+       int min_of_block = get_min_of_block(arr, block_number, block_length);
+       sqrt_memory[block_number] = min_of_block;
+    }
+}
+
+int get_min(int* arr, int size, int left_index, int right_index, int* sqrt_memory)
+{
+    int left_up = 1;//the block number that touched by left_index
+    int right_down = 3;//the block number that touched by right_index
+    
+
+
+    return 0;
+}
+
+int main()
+{
     //int arr[]{ 14, 16, 19, 30, 32, 33, 56, 69, 72 };
     int arr[]{ 1, 5, 2,   4, 6, 1,   4, 5, 7 };
     int size = sizeof(arr) / sizeof(arr[0]);
     int left_index = 1;//задача найти минимальный элемент между индексом 1 включительно и по правой границе индекс 7 не включительно
     int right_index = 7;
 
-    const int block_length = 3;//we decided that one block has 3 elements //мы сами решили что пусть в одном блоке 3 элемента
-    int blocks_quantity = size / block_length;//
+    const int block_length = 3;//we decided that one block has 3 elements beacuse 3 is the sqrt of 9//мы сами решили что пусть в одном блоке 3 элемента тк 3 это корень из 9
+    int blocks_quantity = size / block_length;//blocks quantity of 9 elements is 3 blocks
     int* sqrt_memory = new int[blocks_quantity];
+    preprocessing(arr, sqrt_memory, blocks_quantity, block_length);
 
-    int min = getMin(arr, size, left_index, right_index, sqrt_memory);
+    
+    int min = get_min(arr, size, left_index, right_index, sqrt_memory);
+
     std::cout << "SQRT decomposition" << min << ": " ;
     delete[] sqrt_memory;
 }
